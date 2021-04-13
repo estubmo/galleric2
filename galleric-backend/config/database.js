af9926de51d955bee9dc88364fb16f542e31a70a
Eirik Mo <eirik_stub@hotmail.com>
2021-04-13 22:09:27 +0100

    Change to PostgreSQL for prod

--- galleric-backend/config/database.js
+++ galleric-backend/config/database.js
@@ -1,20 +1,26 @@
-module.exports = ({ env }) => ({
-    defaultConnection: "default",
-    connections: {
-        default: {
-            connector: "mongoose",
-            settings: {
-                client: "mongo",
-                host: env("DATABASE_HOST", "localhost"),
-                port: env.int("DATABASE_PORT", 27017),
-                database: env("DATABASE_NAME", "strapi"),
-                username: env("DATABASE_USERNAME", "strapi"),
-                password: env("DATABASE_PASSWORD", "strapi"),
-            },
-            options: {
-                authenticationDatabase: env("AUTHENTICATION_DATABASE"),
-                ssl: env("DATABASE_SSL"),
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+    return {
+        defaultConnection: "default",
+        connections: {
+            default: {
+                connector: "bookshelf",
+                settings: {
+                    client: "postgres",
+                    host,
+                    port,
+                    database,
+                    username: user,
+                    password,
+                    ssl: { rejectUnauthorized: false },
+                },
+                options: {
+                    ssl: false,
+                },
             },
         },
-    },
-});
+    };
+};
