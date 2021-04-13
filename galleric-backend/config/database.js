c8a8cd7cdace56da016e9972e1f0251942a0cfbe
Eirik Mo <eirik_stub@hotmail.com>
2021-04-13 20:10:16 +0100

    Change DB to MongoDB

--- galleric-backend/config/database.js
+++ galleric-backend/config/database.js
@@ -1,15 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
+    defaultConnection: "default",
+    connections: {
+        default: {
+            connector: "mongoose",
+            settings: {
+                client: "mongo",
+                host: env("DATABASE_HOST", "localhost"),
+                port: env.int("DATABASE_PORT", 27017),
+                database: env("DATABASE_NAME", "strapi"),
+                username: env("DATABASE_USERNAME", "strapi"),
+                password: env("DATABASE_PASSWORD", "strapi"),
+            },
+            options: {
+                authenticationDatabase: env("AUTHENTICATION_DATABASE"),
+                ssl: env("DATABASE_SSL"),
+            },
+        },
     },
-  },
 });
---
+++ galleric-backend/config/plugins.js
@@ -0,0 +1,18 @@
+const { env } = require("strapi-utils/lib");
+module.exports = ({ env }) => ({
+    email: {
+        provider: env("EMAIL_PROVIDER"),
+        providerOptions: {
+            host: env("EMAIL_SMTP_HOST", "smtp.example.com"),
+            port: env("EMAIL_SMTP_PORT", 587),
+            auth: {
+                user: env("EMAIL_SMTP_USER"),
+                pass: env("EMAIL_SMTP_PASS"),
+            },
+        },
+        settings: {
+            defaultFrom: env("EMAIL_ADDRESS_FROM"),
+            defaultReplyTo: env("EMAIL_ADDRESS_REPLY"),
+        },
+    },
+});
