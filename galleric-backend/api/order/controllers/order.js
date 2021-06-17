eaf080647707ba8b2077bcee483b2a9b28f6a1a4
Eirik Mo <eirik_stub@hotmail.com>
2021-06-17 03:52:21 +0200

    Add order content model

---
+++ galleric-backend/api/order/controllers/order.js
@@ -0,0 +1,47 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  /** Retrieve orders, as long as it belongs to the user.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    const { user } = ctx.state;
+
+    let entities;
+    if ({ ...ctx.query, user: user.id }._q) {
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    } else {
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
+  },
+  /** Returns one order, as long as it belongs to the user.id
+   *
+   * @return {any}
+   */
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+
+    const entity = await strapi.services.order.findOne({ id, user: user.id });
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
