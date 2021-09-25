e59f2eb4b29913515cc10760dd541ebdfc3c9986
Eirik Mo <eirik_stub@hotmail.com>
2021-09-26 00:30:25 +0200

    Add fallback for no .env

--- galleric-backend/config/database.js
+++ galleric-backend/config/database.js
@@ -1,5 +1,5 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const config = process.env.DATABASE_URL ? parse(process.env.DATABASE_URL) : {};
 
 //CI test
 module.exports = ({ env }) => ({
@@ -9,11 +9,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: config?.host,
+        port: config?.port,
+        database: config?.database,
+        username: config?.user,
+        password: config?.password,
         ssl: {
           rejectUnauthorized: false,
         },
