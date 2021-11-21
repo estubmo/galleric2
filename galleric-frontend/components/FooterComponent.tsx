de49e796990dd42f38eaf70ad2e8c7141cee506c
Eirik Mo <eirik_stub@hotmail.com>
2021-11-21 19:11:14 +0100

    Add mail links

--- galleric-frontend/components/FooterComponent.tsx
+++ galleric-frontend/components/FooterComponent.tsx
@@ -26,9 +26,19 @@ const FooterComponent = ({ wallpaperUrl }: IFooterComponent): JSX.Element => {
                 viewport={{ once: true, amount: 0.8 }}
                 className="relative z-30 flex flex-col items-center justify-center w-full h-screen max-w-sm space-y-1 text-sm font-normal text-center bg-black md:max-w-screen-sm xl:max-w-screen-lg bg-opacity-20 md:space-y-4">
                 <div>Svanhild Stub</div>
-                <div>gallery@svanhildstub.com </div>
+                <div>
+                    <a className="hover:underline" href="mailto:gallery@svanhildstub.com">
+                        gallery@svanhildstub.com
+                    </a>
+                </div>
                 <div>All paintings courtesy of the artist</div>
-                <div>Site by Mo Web Dev © 2021</div>
+                <div>
+                    Site by{' '}
+                    <a className="hover:underline" href="mailto:eirik@mowebdev.com">
+                        Mo Web Dev
+                    </a>{' '}
+                    © 2021
+                </div>
             </motion.div>
             {wallpaperUrl && (
                 <Image
